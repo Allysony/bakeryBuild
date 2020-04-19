c0e983db80085893322dc3fc94de718b0c1c6fb7
Allyson Yamasaki <allyson.yamasaki@gmail.com>
2020-04-19 12:20:28 -0700

    i hate everything

---
+++ precache-manifest.52db3338bd4e65e6257067900c6b52bc.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a35001f311d1b11fe2edf4580d45126",
+    "url": "/bakeryBuild/index.html"
+  },
+  {
+    "revision": "49aa1ef83b4e9d15fe24",
+    "url": "/bakeryBuild/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "5f44007e9316c9e2e543",
+    "url": "/bakeryBuild/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "49aa1ef83b4e9d15fe24",
+    "url": "/bakeryBuild/static/js/2.a3d775cd.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/bakeryBuild/static/js/2.a3d775cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f44007e9316c9e2e543",
+    "url": "/bakeryBuild/static/js/main.c83333b2.chunk.js"
+  },
+  {
+    "revision": "83e1f00494d47d1dea34",
+    "url": "/bakeryBuild/static/js/runtime-main.6d5894e5.js"
+  },
+  {
+    "revision": "3c5350ce2bae0e9e019b4fa820f89619",
+    "url": "/bakeryBuild/static/media/bakers.3c5350ce.png"
+  },
+  {
+    "revision": "c5a9be50b4aa41b26ad433e66a24b981",
+    "url": "/bakeryBuild/static/media/chinatown.c5a9be50.jpg"
+  },
+  {
+    "revision": "800ff8186d758ae09d15bad4659d1fae",
+    "url": "/bakeryBuild/static/media/cupcake.800ff818.png"
+  },
+  {
+    "revision": "3beb662424b71ef7fb874999ebd3a264",
+    "url": "/bakeryBuild/static/media/downtown.3beb6624.jpg"
+  },
+  {
+    "revision": "bf41f737469c45042faff1af24ded5c5",
+    "url": "/bakeryBuild/static/media/kyoto.bf41f737.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/bakeryBuild/static/media/logo.5d5d9eef.svg"
+  }
+]);
