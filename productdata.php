a009f7cfe3220943ad7221d3d041a95282fd37bf
Richard Liu <richardbliu@gmail.com>
2020-05-03 15:59:08 -0700

    Add files via upload

---
+++ productdata.php
@@ -0,0 +1,24 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+
+
+$host = "localhost";
+$user = "root"; 
+$password = "";
+$dbname = "vanillajs";
+$con = mysqli_connect($host, $user, $password, $dbname);
+
+$selectsql = "SELECT * FROM Products";
+$selectresult = mysqli_query($con,$selectsql);
+
+$rows = array();
+if (mysqli_num_rows($selectresult) > 0) {
+    while($row = mysqli_fetch_assoc($selectresult)) {
+        $rows[] = $row;
+    }
+    echo json_encode($rows);
+}
+
+
+
+?>
---
+++ taxrates.php
@@ -0,0 +1,6 @@
+<?php
+	header("Access-Control-Allow-Origin: *");
+	header("Access-Control-Allow-Headers: Content-Type");
+	header("Content-Type: application/json");
+	echo file_get_contents("taxrates.json")
+?>
