b8070843eb2ffad58fa9f1b27ce221345fbaf14a
Allyson Yamasaki <allyson.yamasaki@gmail.com>
2020-05-11 13:28:32 -0700

    updated build basename urls

---
+++ project2/precache-manifest.fe46ef2fe7a48957600923fe79ee9c6d.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08dedc37631d9b6e3d94bfb57ddcda2c",
+    "url": "/project2/index.html"
+  },
+  {
+    "revision": "699f095c65aded4e45be",
+    "url": "/project2/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "0c45e6370264c7d95438",
+    "url": "/project2/static/css/main.13401b55.chunk.css"
+  },
+  {
+    "revision": "699f095c65aded4e45be",
+    "url": "/project2/static/js/2.f4137242.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/project2/static/js/2.f4137242.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c45e6370264c7d95438",
+    "url": "/project2/static/js/main.ac7af246.chunk.js"
+  },
+  {
+    "revision": "7abce460ff5add4a8449",
+    "url": "/project2/static/js/runtime-main.d8b0a9c7.js"
+  },
+  {
+    "revision": "3c5350ce2bae0e9e019b4fa820f89619",
+    "url": "/project2/static/media/bakers.3c5350ce.png"
+  },
+  {
+    "revision": "edf9f2d6020da0ac032f66d76dd8e0fa",
+    "url": "/project2/static/media/carousel_cookies.edf9f2d6.png"
+  },
+  {
+    "revision": "184d52548f5cceb8de9ae7c5b392ebf7",
+    "url": "/project2/static/media/carousel_delivery.184d5254.png"
+  },
+  {
+    "revision": "05b03559b5fa0e7d8031a1a6f069c5db",
+    "url": "/project2/static/media/carousel_update.05b03559.png"
+  },
+  {
+    "revision": "800ff8186d758ae09d15bad4659d1fae",
+    "url": "/project2/static/media/cupcake.800ff818.png"
+  },
+  {
+    "revision": "a6dc13b728241ea8658c3edd5bde1ce8",
+    "url": "/project2/static/media/logo.a6dc13b7.png"
+  }
+]);
--- project2/service-worker.js
+++ project2/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/bakeryBuild/precache-manifest.949bf2c73030fd1c847650dd87b31b52.js"
+  "/project2/precache-manifest.fe46ef2fe7a48957600923fe79ee9c6d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/bakeryBuild/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/project2/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
